21e1b69aa1ca027d26a9b6a3db47f315bcb11294
Januzaj <preetg@iitk.ac.in>
2016-01-16 17:40:01 +0530

    removed few tokens

--- lexer.py
+++ lexer.py
@@ -104,10 +104,11 @@ reserved = {
 ############# TOKENS ###################
 ########################################
 tokens = [
-        'DIGIT','HEXDIGIT', 'OCTDIGIT' , 'BINDIGIT', 'HEX_LIT', 'DEC_LIT', 'OCT_LIT', 'BIN_LIT', 'EXPONENT', 'SYM_CHARS', 'SYM_START_CHARS', 'INVALID', 'EOF', 'SYMBOL', 'INTLIT', 'INT8LIT', 'INT16LIT', 'INT32LIT', 'INT64LIT', 'UINTLIT', 'UINT8LIT',
-    'UINT16LIT', 'UINT32LIT', 'UINT64LIT', 'FLOATLIT', 'FLOAT32LIT', 'FLOAT64LIT', 'FLOAT128LIT', 'STRLIT', 'RSTRLIT', 'TRIPLESTRLIT', 'PARLE', 'PARRI',
-    'BRACKETLE', 'BRACKETRI', 'CURLYLE', 'CURLYRI', 'BRACKETDOTLE', 'BRACKETDOTRI', 'CURLYDOTLE', 'CURLYDOTRI', 'PARDOTLE', 'PARDOTRI', 'COMMA', 'SEMICOLON',   
-    'COLON', 'COLONCOLON', 'EQUALS', 'DOT', 'DOTDOT', 'OPR', 'COMMENT', 'ACCENT', 'IDENTIFIER'
+        'DIGIT','HEXDIGIT', 'OCTDIGIT' , 'BINDIGIT', 'HEX_LIT', 'DEC_LIT', 'OCT_LIT', 'BIN_LIT', 'EXPONENT', 'SYM_CHARS', 'SYM_START_CHARS', 'INVALID', 'EOF', 'SYMBOL',
+         'INTLIT', 'INT8LIT', 'INT16LIT', 'INT32LIT', 'INT64LIT', 'UINTLIT', 'UINT8LIT',
+        'UINT16LIT', 'UINT32LIT', 'UINT64LIT', 'FLOATLIT', 'FLOAT32LIT', 'FLOAT64LIT', 'FLOAT128LIT', 'STRLIT', 'RSTRLIT', 'TRIPLESTRLIT', 'PARLE', 'PARRI',
+        'BRACKETLE', 'BRACKETRI', 'CURLYLE', 'CURLYRI', 'BRACKETDOTLE', 'BRACKETDOTRI', 'CURLYDOTLE', 'CURLYDOTRI', 'PARDOTLE', 'PARDOTRI', 'COMMA', 'SEMICOLON',   
+        'COLON', 'COLONCOLON', 'EQUALS', 'DOT', 'DOTDOT', 'OPR', 'COMMENT', 'ACCENT', 'IDENTIFIER', 'NUMBER'
         ] + list(reserved.values())
 
 ########################################
@@ -141,10 +142,10 @@ def t_OPR(t):
     r"+|"r"-|"r"*|"r"/|"r"\\|"r"<|"r">|"r"!|"r"?|"r"\^|"r"\||"r"%|"r"&|"r"$|"r"@|"r"~"
     return t
     
-def t_STRING(t):
-    r"(?P<start>\"|')[^\"']*(?P=start)"
-    t.value = t.value.replace("\"", "").replace("'", "")
-    return t
+# def t_STRING(t):
+#     r"(?P<start>\"|')[^\"']*(?P=start)"
+#     t.value = t.value.replace("\"", "").replace("'", "")
+#     return t
 
 def t_NUMBER(t):
     r"\d+"
@@ -199,78 +200,78 @@ if __name__ == "__main__":
     testLex(inputFile)
 
                                       
-        DIGIT           = r'([0-9])'
-        HEXDIGIT        = r'({DIGIT}|[A-F]|[a-f])'
-        OCTDIGIT        = r'([0-7])'
-        BINDIGIT        = r'([0-1])'
-        HEX_LIT         = r'(0(x|X){HEXDIGIT}(_{HEXDIGIT})*)'
-        DEC_LIT         = r'(-?{DIGIT}(_{DIGIT})*)'
-        OCT_LIT         = r'(0o{OCTDIGIT}(_{OCTDIGIT})*)'
-        BIN_LIT         = r'(0(b|B){BINDIGIT}(_{BINDIGIT})*)'
-        EXPONENT        = r'((e|E)[+-]{DIGIT}(_{DIGIT})*)'
-        SYM_CHARS       = r'([a-zA-Z0-9\x80-\xFF_]+)'
-        SYM_START_CHARS = r'([a-zA-Z\x80-\xFF]+)'
-        //////////////////////////////////////////////////////////////////////////////
+        # DIGIT           = r'([0-9])'
+        # HEXDIGIT        = r'({DIGIT}|[A-F]|[a-f])'
+        # OCTDIGIT        = r'([0-7])'
+        # BINDIGIT        = r'([0-1])'
+        # HEX_LIT         = r'(0(x|X){HEXDIGIT}(_{HEXDIGIT})*)'
+        # DEC_LIT         = r'(-?{DIGIT}(_{DIGIT})*)'
+        # OCT_LIT         = r'(0o{OCTDIGIT}(_{OCTDIGIT})*)'
+        # BIN_LIT         = r'(0(b|B){BINDIGIT}(_{BINDIGIT})*)'
+        # EXPONENT        = r'((e|E)[+-]{DIGIT}(_{DIGIT})*)'
+        # SYM_CHARS       = r'([a-zA-Z0-9\x80-\xFF_]+)'
+        # SYM_START_CHARS = r'([a-zA-Z\x80-\xFF]+)'
+        # //////////////////////////////////////////////////////////////////////////////
 
-//        INVALID       = "tkInvalid"
-//        EOF           = "[EOF]"
-        SYMBOL         = r'({SYM_START_CHARS}{SYM_CHARS}")'
-        INTLIT         = r'({HEX_LIT}|{DEC_LIT}|{OCT_LIT}|{BIN_LIT}")'
-        INT8LIT        = r'({INTLIT}'[iI]8")'
-        INT16LIT       = r'({INTLIT}'[iI]16")'
-        INT32LIT       = r'({INTLIT}'[iI]32")'
-        INT64LIT       = r'({INTLIT}'[iI]64")'
-        UINTLIT        = r'({INTLIT}'[uU]")'
-        UINT8LIT       = r'({UINTLIT}'[uU]8")'
-        UINT16LIT      = r'({INTLIT}'[uU]16")'
-        UINT32LIT      = r'({INTLIT}'[uU]32")'
-        UINT64LIT      = r'({INTLIT}'[uU]64")'
-        FLOATLIT       = r'(-?{DIGIT}(_{DIGIT})*((.(_{DIGIT})*[EXPONENT])|{EXPONENT})")'
-        FLOAT32LIT     = r'(({HEX_LIT}|{FLOATLIT}|{DEC_LIT}|{OCT_LIT}|{BIN_LIT})'[fF]32")'
-        FLOAT64LIT     = r'(({HEX_LIT}|{FLOATLIT}|{DEC_LIT}|{OCT_LIT}|{BIN_LIT})'[fF]64")'
-        FLOAT128LIT    = r'(({HEX_LIT}|{FLOATLIT}|{DEC_LIT}|{OCT_LIT}|{BIN_LIT})'[fF]128")'
-        STRLIT         = r'("(\\"|\\[^"]|[^\\])*")'
-        RSTRLIT        = r'(r{STRLIT}")'
-        TRIPLESTRLIT   = r'(\"""(.|{EOL})*\""")'
-//        GSTRLIT        = "tkGStrLit"
-//        GTRIPLESTRLIT  = "tkGTripleStrLit"
-//        CHARLIT        = "tkCharLit"
+# //        INVALID       = "tkInvalid"
+# //        EOF           = "[EOF]"
+#         SYMBOL         = r'({SYM_START_CHARS}{SYM_CHARS}")'
+#         INTLIT         = r'({HEX_LIT}|{DEC_LIT}|{OCT_LIT}|{BIN_LIT}")'
+#         INT8LIT        = r'({INTLIT}'[iI]8")'
+#         INT16LIT       = r'({INTLIT}'[iI]16")'
+#         INT32LIT       = r'({INTLIT}'[iI]32")'
+#         INT64LIT       = r'({INTLIT}'[iI]64")'
+#         UINTLIT        = r'({INTLIT}'[uU]")'
+#         UINT8LIT       = r'({UINTLIT}'[uU]8")'
+#         UINT16LIT      = r'({INTLIT}'[uU]16")'
+#         UINT32LIT      = r'({INTLIT}'[uU]32")'
+#         UINT64LIT      = r'({INTLIT}'[uU]64")'
+#         FLOATLIT       = r'(-?{DIGIT}(_{DIGIT})*((.(_{DIGIT})*[EXPONENT])|{EXPONENT})")'
+#         FLOAT32LIT     = r'(({HEX_LIT}|{FLOATLIT}|{DEC_LIT}|{OCT_LIT}|{BIN_LIT})'[fF]32")'
+#         FLOAT64LIT     = r'(({HEX_LIT}|{FLOATLIT}|{DEC_LIT}|{OCT_LIT}|{BIN_LIT})'[fF]64")'
+#         FLOAT128LIT    = r'(({HEX_LIT}|{FLOATLIT}|{DEC_LIT}|{OCT_LIT}|{BIN_LIT})'[fF]128")'
+#         STRLIT         = r'("(\\"|\\[^"]|[^\\])*")'
+#         RSTRLIT        = r'(r{STRLIT}")'
+#         TRIPLESTRLIT   = r'(\"""(.|{EOL})*\""")'
+# //        GSTRLIT        = "tkGStrLit"
+# //        GTRIPLESTRLIT  = "tkGTripleStrLit"
+# //        CHARLIT        = "tkCharLit"
 
 #Delimiters
-        PARLE          = r'\('
-        PARRI          = r'\)'
-        BRACKETLE      = r'\['
-        BRACKETRI      = r'\]'
-        CURLYLE        = r'\{'
-        CURLYRI        = r'\}'
-        BRACKETDOTLE   = r'\[\.'
-        BRACKETDOTRI   = r'\.\['
-        CURLYDOTLE     = r'\{\.'
-        CURLYDOTRI     = r'\.\}'
-        PARDOTLE       = r'\(\.'
-        PARDOTRI       = r'\.\)'
-        COMMA          = r'\,'
-        SEMICOLON      = r';'
-        COLON          = r':'
-        COLONCOLON     = r'::'
-        EQUALS         = r'='
-        DOT            = r'\.'
-        DOTDOT         = r'\.\.'
+        t_PARLE          = r'\('
+        t_PARRI          = r'\)'
+        t_BRACKETLE      = r'\['
+        t_BRACKETRI      = r'\]'
+        t_CURLYLE        = r'\{'
+        t_CURLYRI        = r'\}'
+        t_BRACKETDOTLE   = r'\[\.'
+        t_BRACKETDOTRI   = r'\.\['
+        t_CURLYDOTLE     = r'\{\.'
+        t_CURLYDOTRI     = r'\.\}'
+        t_PARDOTLE       = r'\(\.'
+        t_PARDOTRI       = r'\.\)'
+        t_COMMA          = r'\,'
+        t_SEMICOLON      = r';'
+        t_COLON          = r':'
+        t_COLONCOLON     = r'::'
+        t_EQUALS         = r'='
+        t_DOT            = r'\.'
+        t_DOTDOT         = r'\.\.'
 
 
-        OPR            = r'([+-*/\\<>!?\^.|=%&$@~:\x80-\xFF]")'
-#        COMMENT        = r'(#[^\r\n]*")'
-        ACCENT         = "`"
-//        INFIXOPR       = "tkInfixOpr"
-//        PREFIXOPR      = "tkPrefixOpr"
-//        POSTFIXOPR     = "tkPostfixOpr"
-    ]
-}
+        #OPR            = r'([+-*/\\<>!?\^.|=%&$@~:\x80-\xFF]")'
+# #        COMMENT        = r'(#[^\r\n]*")'
+#         ACCENT         = "`"
+# //        INFIXOPR       = "tkInfixOpr"
+# //        PREFIXOPR      = "tkPrefixOpr"
+# //        POSTFIXOPR     = "tkPostfixOpr"
+#     ]
 
-input ::= (SYMBOL|ADDR|AND|AS|ASM|ATOMIC|BIND|BLOCK|BREAK|CASE|CAST|CONCEPT|CONST|CONTINUE|CONVERTER|DEFER|DISCARD|
-DISTINCT|DIV|DO|ELIF|ELSE|END|ENUM|EXCEPT|EXPORT|FINALLY|FOR|FROM|FUNC|GENERIC|IF|IMPORT|IN|INCLUDE|INTERFACE|IS|ISNOT|
-ITERATOR|LET|MACRO|METHOD|MIXIN|MOD|NIL|NOT|NOTIN|OBJECT|OF|OR|OUT|PROC|PTR|RAISE|REF|RETURN|SHL|SHR|STATIC|TEMPLATE|
-TRY|TUPLE|TYPE|USING|VAR|WHEN|WHILE|WITH|WITHOUT|XOR|YIELD|INTLIT|INT8LIT|INT16LIT|INT32LIT|INT64LIT|UINTLIT|UINT8LIT|
-UINT16LIT|UINT32LIT|UINT64LIT|FLOATLIT|FLOAT32LIT|FLOAT64LIT|FLOAT128LIT|STRLIT|RSTRLIT|TRIPLESTRLIT|PARLE|PARRI|
-BRACKETLE|BRACKETRI|CURLYLE|CURLYRI|BRACKETDOTLE|BRACKETDOTRI|CURLYDOTLE|CURLYDOTRI|PARDOTLE|PARDOTRI|COMMA|SEMICOLON|
-COLON|COLONCOLON|EQUALS|DOT|DOTDOT|OPR|COMMENT|ACCENT)*
+
+# input ::= (SYMBOL|ADDR|AND|AS|ASM|ATOMIC|BIND|BLOCK|BREAK|CASE|CAST|CONCEPT|CONST|CONTINUE|CONVERTER|DEFER|DISCARD|
+# DISTINCT|DIV|DO|ELIF|ELSE|END|ENUM|EXCEPT|EXPORT|FINALLY|FOR|FROM|FUNC|GENERIC|IF|IMPORT|IN|INCLUDE|INTERFACE|IS|ISNOT|
+# ITERATOR|LET|MACRO|METHOD|MIXIN|MOD|NIL|NOT|NOTIN|OBJECT|OF|OR|OUT|PROC|PTR|RAISE|REF|RETURN|SHL|SHR|STATIC|TEMPLATE|
+# TRY|TUPLE|TYPE|USING|VAR|WHEN|WHILE|WITH|WITHOUT|XOR|YIELD|INTLIT|INT8LIT|INT16LIT|INT32LIT|INT64LIT|UINTLIT|UINT8LIT|
+# UINT16LIT|UINT32LIT|UINT64LIT|FLOATLIT|FLOAT32LIT|FLOAT64LIT|FLOAT128LIT|STRLIT|RSTRLIT|TRIPLESTRLIT|PARLE|PARRI|
+# BRACKETLE|BRACKETRI|CURLYLE|CURLYRI|BRACKETDOTLE|BRACKETDOTRI|CURLYDOTLE|CURLYDOTRI|PARDOTLE|PARDOTRI|COMMA|SEMICOLON|
+# COLON|COLONCOLON|EQUALS|DOT|DOTDOT|OPR|COMMENT|ACCENT)*
