a0429f3ca33ef7c43bad2398f53812516d92583b
VivekVerma <vivek@cse.iitk.ac.in>
2016-01-19 19:29:37 +0530

    extra tokenized

--- lexer.py
+++ lexer.py
@@ -148,9 +148,11 @@ t_ACCENT         = r'`'
 def t_MULTICOMMENT(t):
     r"[\s ]*\#\[[^\]]*[^\#]*\]\#\n"
     pass
+
 def t_COMMENT(t):
     r"[\s ]*\#[^\n]*\n"  # \043 is '#'
     pass
+
 ########################################
 ############# LINE NUMBER ##############
 ########################################
@@ -171,14 +173,14 @@ def t_NEWLINE(t):
 
 # Whitespace
 def t_WSI(t):
-    r' (?<=\n)[\s ]+ '
+    r' ((?<=\n)[\s ]+)|(^[\t]+) '
     #if t.lexer.at_line_start and t.lexer.paren_count == 0:
     return t
 
 # Whitespace
 def t_WS(t):
     r' [\s ]+ '
-    return t
+    pass
 
 
 ########################################
@@ -199,23 +201,47 @@ def t_OPR(t):
 #     t.value = t.value.replace("\"", "").replace("'", "")
 #     return t
 
-def t_NUMBER(t):
-    r"\d+"
-    t.value = int(t.value)
-    return t
+def t_INTLIT(t):
+	r'((0(x|X)[0-9A-Fa-f]+)|(0(o|O)[0-7]+)|(0(b|B)[0-1]]+)|\d+)'
+	return t
+
+# def t_HEX_LIT(t):
+# 	r"(0(x|X)[0-9A-Fa-f]+)"
+# 	return t
+
+# def t_OCT_LIT(t):
+# 	r"(0(o|O)[0-7]+)"
+# 	return t
+
+# def t_BIN_LIT(t):
+#     r'(0(b|B)[0-1]]+)'
+#     t.value = int(t.value)
+#     return t
+
+# def t_DEC_LIT(t):
+#     r'\d+'
+#     t.value = int(t.value)
+#     return t
 
 def t_TRIPLESTRLIT(t):
     r'(\"\"\"[^(\"\"\")]*\"\"\")'
     return t
 
-def t_STRLIT(t):
-    r'(\"[^(\")]*\")'
+def t_RSTRLIT(t):
+    r'r(\"[^(\")]*\")'  # I think this is right ...
+    #r''
+    #t.value=t.value[1:-1].decode("string-escape") # .swapcase() # for fun
     return t
 
-# def t_STRLIT(t):
-#     r'"([^\"]+|\\"|\\\\)*"'  # I think this is right ...
-#     #t.value=t.value[1:-1] # .swapcase() # for fun
-#     return t
+def t_STRLIT(t):
+    r'"([^\n\\"]+|\\"|\\\\)*"'  # I think this is right ...
+    #r''
+    #t.value=t.value[1:-1].decode("string-escape") # .swapcase() # for fun
+    return t
+
+def t_EXPONENT(t):
+	r'([e|E][+-][0-9]+)'
+	return t;
 
 def t_BOOLEAN(t):
     r"true|false"
@@ -230,157 +256,59 @@ def t_IDENTIFIER(t):
     return t
 
 
+# def t_SYM_CHARS(t):
+# 	r'([a-zA-Z0-9\x80-\xFF_]+)'
+# 	return t
+
+# def t_SYM_START_CHARS(t):
+# 	r'([a-zA-Z\x80-\xFF]+)'
+# 	return t
+
+#SYMBOL         = r'({SYM_START_CHARS}{SYM_CHARS}")'
+ 
+#INTLIT         = r'((0(x|X)[0-9A-Fa-f]+)|(0(o|O)[0-7]+)|(0(b|B)[0-1]]+)|\d+)'
+#INT8LIT        = r'({INTLIT}'[iI]8")'
+#INT16LIT       = r'({INTLIT}'[iI]16")'
+#INT32LIT       = r'({INTLIT}'[iI]32")'
+#INT64LIT       = r'({INTLIT}'[iI]64")'
+#UINTLIT        = r'({INTLIT}'[uU]")'
+#UINT8LIT       = r'({UINTLIT}'[uU]8")'
+#UINT16LIT      = r'({INTLIT}'[uU]16")'
+#UINT32LIT      = r'({INTLIT}'[uU]32")'
+#UINT64LIT      = r'({INTLIT}'[uU]64")'
+#FLOATLIT       = r'(-?{DIGIT}(_{DIGIT})*((.(_{DIGIT})*[EXPONENT])|{EXPONENT})")'
+#FLOAT32LIT     = r'(({HEX_LIT}|{FLOATLIT}|{DEC_LIT}|{OCT_LIT}|{BIN_LIT})'[fF]32")'
+#FLOAT64LIT     = r'(({HEX_LIT}|{FLOATLIT}|{DEC_LIT}|{OCT_LIT}|{BIN_LIT})'[fF]64")'
+#FLOAT128LIT    = r'(({HEX_LIT}|{FLOATLIT}|{DEC_LIT}|{OCT_LIT}|{BIN_LIT})'[fF]128")'
+
 
 ########################################
 ############# ERROR ####################
 ########################################
 def t_error(t):
     print "Illegal character '%s'" % t.value[0]
+    tok_data.setdefault('ILLEGAL_CHARACTERS', list())
+    tok_data['ILLEGAL_CHARACTERS'].append(str(t.value[0]))
     t.lexer.skip(1)
 
-######################################################################################################
-# Create a lexer which uses the above defined rules, this can be used by the any parser which
-# includes this file
-
-######### Required Globals #############
-#debug = debug.Debug()
 lexer = lex.lex()
 prev = 0
-########################################
-
-#Simple testing
-# data = '''
-# !3=4+10$~abcd
-#  =20
-#  '''
 
 with open(sys.argv[1], 'r') as my_file:
-    # print(my_file.read())
+    
     lexer.input(my_file.read())
 
-# # Give the lexer some input
-# lexer.input(data)
+tok_data = {};
 
-# Tokenize
 while True:
     tok = lexer.token()
     if not tok: 
         break      # No more input
+    tok_data.setdefault(tok.type, list())
+    tok_data[tok.type].append(str(tok.value));
+
     print(tok)
 
-
-# # A function to test the lexer
-# def testLex(inputFile):
-#     # Open the passed argument as an input file and then pass it to lex
-#     program = open(inputFile).read()
-#     lexer.input(program)
-
-#     # This iterates over the function lex.token and converts the returned object into an iterator
-#     print "\tTYPE \t\t\t\t\t\t VALUE"
-#     print "\t---- \t\t\t\t\t\t -----"
-#     for tok in iter(lexer.token, None):
-#         print "%-25s \t\t\t\t %s" %(repr(tok.type), repr(tok.value))
-
-# if __name__ == "__main__":
-#     from sys import argv
-#     filename, inputFile = argv
-#     testLex(inputFile)
-
-                                      
-        # DIGIT           = r'([0-9])'
-        # HEXDIGIT        = r'({DIGIT}|[A-F]|[a-f])'
-        # OCTDIGIT        = r'([0-7])'
-        # BINDIGIT        = r'([0-1])'
-        # HEX_LIT         = r'(0(x|X){HEXDIGIT}(_{HEXDIGIT})*)'
-        # DEC_LIT         = r'(-?{DIGIT}(_{DIGIT})*)'
-        # OCT_LIT         = r'(0o{OCTDIGIT}(_{OCTDIGIT})*)'
-        # BIN_LIT         = r'(0(b|B){BINDIGIT}(_{BINDIGIT})*)'
-        # EXPONENT        = r'((e|E)[+-]{DIGIT}(_{DIGIT})*)'
-        # SYM_CHARS       = r'([a-zA-Z0-9\x80-\xFF_]+)'
-        # SYM_START_CHARS = r'([a-zA-Z\x80-\xFF]+)'
-        # //////////////////////////////////////////////////////////////////////////////
-
-# //        INVALID       = "tkInvalid"
-# //        EOF           = "[EOF]"
-#         SYMBOL         = r'({SYM_START_CHARS}{SYM_CHARS}")'
-#         INTLIT         = r'({HEX_LIT}|{DEC_LIT}|{OCT_LIT}|{BIN_LIT}")'
-#         INT8LIT        = r'({INTLIT}'[iI]8")'
-#         INT16LIT       = r'({INTLIT}'[iI]16")'
-#         INT32LIT       = r'({INTLIT}'[iI]32")'
-#         INT64LIT       = r'({INTLIT}'[iI]64")'
-#         UINTLIT        = r'({INTLIT}'[uU]")'
-#         UINT8LIT       = r'({UINTLIT}'[uU]8")'
-#         UINT16LIT      = r'({INTLIT}'[uU]16")'
-#         UINT32LIT      = r'({INTLIT}'[uU]32")'
-#         UINT64LIT      = r'({INTLIT}'[uU]64")'
-#         FLOATLIT       = r'(-?{DIGIT}(_{DIGIT})*((.(_{DIGIT})*[EXPONENT])|{EXPONENT})")'
-#         FLOAT32LIT     = r'(({HEX_LIT}|{FLOATLIT}|{DEC_LIT}|{OCT_LIT}|{BIN_LIT})'[fF]32")'
-#         FLOAT64LIT     = r'(({HEX_LIT}|{FLOATLIT}|{DEC_LIT}|{OCT_LIT}|{BIN_LIT})'[fF]64")'
-#         FLOAT128LIT    = r'(({HEX_LIT}|{FLOATLIT}|{DEC_LIT}|{OCT_LIT}|{BIN_LIT})'[fF]128")'
-#STRLIT implemented 
-#         STRLIT         = r'("(\\"|\\[^"]|[^\\])*")'
-#         RSTRLIT        = r'(r{STRLIT}")'
-#         TRIPLESTRLIT   = r'(\"""(.|{EOL})*\""")'
-# //        GSTRLIT        = "tkGStrLit"
-# //        GTRIPLESTRLIT  = "tkGTripleStrLit"
-# //        CHARLIT        = "tkCharLit"
-#         DIGIT           = r'([0-9])'
-#         HEXDIGIT        = r'({DIGIT}|[A-F]|[a-f])'
-#         OCTDIGIT        = r'([0-7])'
-#         BINDIGIT        = r'([0-1])'
-#         HEX_LIT         = r'(0(x|X){HEXDIGIT}(_{HEXDIGIT})*)'
-#         DEC_LIT         = r'(-?{DIGIT}(_{DIGIT})*)'
-#         OCT_LIT         = r'(0o{OCTDIGIT}(_{OCTDIGIT})*)'
-#         BIN_LIT         = r'(0(b|B){BINDIGIT}(_{BINDIGIT})*)'
-#         EXPONENT        = r'((e|E)[+-]{DIGIT}(_{DIGIT})*)'
-#         SYM_CHARS       = r'([a-zA-Z0-9\x80-\xFF_]+)'
-#         SYM_START_CHARS = r'([a-zA-Z\x80-\xFF]+)'
-        
-# #        INVALID       = "tkInvalid"
-# #        EOF           = "[EOF]"
-#         SYMBOL         = r'({SYM_START_CHARS}{SYM_CHARS}")'
-#         INTLIT         = r'({HEX_LIT}|{DEC_LIT}|{OCT_LIT}|{BIN_LIT}")'
-#         INT8LIT        = r'({INTLIT}'[iI]8")'
-#         INT16LIT       = r'({INTLIT}'[iI]16")'
-#         INT32LIT       = r'({INTLIT}'[iI]32")'
-#         INT64LIT       = r'({INTLIT}'[iI]64")'
-#         UINTLIT        = r'({INTLIT}'[uU]")'
-#         UINT8LIT       = r'({UINTLIT}'[uU]8")'
-#         UINT16LIT      = r'({INTLIT}'[uU]16")'
-#         UINT32LIT      = r'({INTLIT}'[uU]32")'
-#         UINT64LIT      = r'({INTLIT}'[uU]64")'
-#         FLOATLIT       = r'(-?{DIGIT}(_{DIGIT})*((.(_{DIGIT})*[EXPONENT])|{EXPONENT})")'
-#         FLOAT32LIT     = r'(({HEX_LIT}|{FLOATLIT}|{DEC_LIT}|{OCT_LIT}|{BIN_LIT})'[fF]32")'
-#         FLOAT64LIT     = r'(({HEX_LIT}|{FLOATLIT}|{DEC_LIT}|{OCT_LIT}|{BIN_LIT})'[fF]64")'
-#         FLOAT128LIT    = r'(({HEX_LIT}|{FLOATLIT}|{DEC_LIT}|{OCT_LIT}|{BIN_LIT})'[fF]128")'
-#         STRLIT         = r'("(\\"|\\[^"]|[^\\])*")'
-#         RSTRLIT        = r'(r{STRLIT}")'
-#         TRIPLESTRLIT   = r'(\"""(.|{EOL})*\""")'
-#        GSTRLIT        = "tkGStrLit"
-#        GTRIPLESTRLIT  = "tkGTripleStrLit"
-#        CHARLIT        = "tkCharLit"
-
-
-
-        #OPR            = r'([+-*/\\<>!?\^.|=%&$@~:\x80-\xFF]")'
-# #        COMMENT        = r'(#[^\r\n]*")'
-#         ACCENT         = "`"
-# //        INFIXOPR       = "tkInfixOpr"
-# //        PREFIXOPR      = "tkPrefixOpr"
-# //        POSTFIXOPR     = "tkPostfixOpr"
-#     ]
-
-#         OPR            = r'([+-*/\\<>!?\^.|=%&$@~:\x80-\xFF]")'
-# #        COMMENT        = r'(#[^\r\n]*")'
-#         ACCENT         = "`"
-#        INFIXOPR       = "tkInfixOpr"
-#        PREFIXOPR      = "tkPrefixOpr"
-#        POSTFIXOPR     = "tkPostfixOpr"
-
-# input ::= (SYMBOL|ADDR|AND|AS|ASM|ATOMIC|BIND|BLOCK|BREAK|CASE|CAST|CONCEPT|CONST|CONTINUE|CONVERTER|DEFER|DISCARD|
-# DISTINCT|DIV|DO|ELIF|ELSE|END|ENUM|EXCEPT|EXPORT|FINALLY|FOR|FROM|FUNC|GENERIC|IF|IMPORT|IN|INCLUDE|INTERFACE|IS|ISNOT|
-# ITERATOR|LET|MACRO|METHOD|MIXIN|MOD|NIL|NOT|NOTIN|OBJECT|OF|OR|OUT|PROC|PTR|RAISE|REF|RETURN|SHL|SHR|STATIC|TEMPLATE|
-# TRY|TUPLE|TYPE|USING|VAR|WHEN|WHILE|WITH|WITHOUT|XOR|YIELD|INTLIT|INT8LIT|INT16LIT|INT32LIT|INT64LIT|UINTLIT|UINT8LIT|
-# UINT16LIT|UINT32LIT|UINT64LIT|FLOATLIT|FLOAT32LIT|FLOAT64LIT|FLOAT128LIT|STRLIT|RSTRLIT|TRIPLESTRLIT|PARLE|PARRI|
-# BRACKETLE|BRACKETRI|CURLYLE|CURLYRI|BRACKETDOTLE|BRACKETDOTRI|CURLYDOTLE|CURLYDOTRI|PARDOTLE|PARDOTRI|COMMA|SEMICOLON|
-# COLON|COLONCOLON|EQUALS|DOT|DOTDOT|OPR|COMMENT|ACCENT)*
+print('\n\nTokens\tOccurances\tLexemes\n')
+for key,value in tok_data.iteritems():
+	print key+"\t\t"+str(len(value))+"\t{"+', '.join(value)+"}"
