2d9122da86f146ff0511bd865dfab142bd453200
preetansh <preetg@iitk.ac.in>
2016-02-01 22:18:56 +0530

    file input

--- codegen.py
+++ codegen.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python
 
 #this program will convert 3-addr code to x86 assembly code
-import 
+import sys
+
+
+if __name__=="__main__": 
+	filename = sys.argv[1]
+	sourcefile = open(filename)
+	code = sourcefile.read()
