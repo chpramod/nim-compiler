5826e95d346e666cec0c5f6a7b8960b9e6ea26d9
preetansh <preetg@iitk.ac.in>
2016-02-02 00:17:11 +0530

    splitting code

--- codegen.py
+++ codegen.py
@@ -4,7 +4,27 @@
 import sys
 
 
+def generateAssCode(code):
+	jumpLabels=["goto","ifgoto","goto","call","label","ret"]
+	label=[]
+	with code as f:
+		for line in f:
+			line=line.rstrip('\n')
+			splitLine=line.split(', ')
+			print splitLine[0]
+			if splitLine[0]=='1':
+				label.append(splitLine[0])
+			elif splitLine[1] in jumpLabels:
+				label.append(splitLine[0])
+	#leaders=getLeaders(code)
+
+
+#def getleaders(code):
+
+
+
 if __name__=="__main__": 
 	filename = sys.argv[1]
 	sourcefile = open(filename)
-	code = sourcefile.read()
+	#print code
+	generateAssCode(sourcefile)
