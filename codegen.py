b44f4c5e6e81747241ef39bbeb1fb65b0f786ff6
preetansh <preetg@iitk.ac.in>
2016-02-02 06:35:36 +0530

    stuck with getting basic blocks

--- codegen.py
+++ codegen.py
@@ -9,11 +9,13 @@ jumpLabels=["goto","ifgoto","call","label","ret"]
 def generateAssCode(code):
 	leaders=[]
 	TAC = []
+	totalLines=0
 	with code as f:
 		for line in f:
+			totalLines+=1
 			line=line.rstrip('\n')
 			splitLine=line.split(', ')
-			#print splitLine[1]
+			TAC.append(splitLine)
 			if splitLine[0]=='1':
 				leaders.append(int(splitLine[0]))
 			elif splitLine[1] in jumpLabels:
@@ -28,22 +30,34 @@ def generateAssCode(code):
 				elif splitLine[1]=='ret':                          #not sure of we need to do this for the 'ret' oper.
 					leaders.append(int(splitLine[0])+1)
 				elif splitLine[1]=='call':
-					for lineIter in f:
-						lineIter=lineIter.rstrip('\n')
-						splitLineIter=lineIter.split(', ')
-						if splitLineIter[1]=='label' and splitLineIter[2]==splitLine[2]:
-							leaders.append(int(splitLineIter[0]))
+					#the following code is flawed, need a solution!
+					# with code as f1:
+					# 	for lineIter in f1:
+					# 		lineIter=lineIter.rstrip('\n')
+					# 		splitLineIter=lineIter.split(', ')
+					# 		if splitLineIter[1]=='label' and splitLineIter[2]==splitLine[2]:
+					# 			leaders.append(int(splitLineIter[0]))
 					leaders.append(int(splitLine[0])+1)
-			TAC.append(splitLine)
-	leaders.sort()		                                
+	leaders.sort()		
+	leaders.remove(totalLines+1)         #removes an entry which is added after reading last line                                
 	pprint.pprint(TAC)
 	pprint.pprint(leaders)
 	processTAC(TAC, leaders)
 
 def processTAC(TAC,leaders):
+	#break code into basic blocks	
 	basicBlocks=[]
-	for i in range(0:len(leaders)-1):
-		
+	for i in range(0,len(leaders)):
+		tempBlock=[]
+		tempBlock.append(leaders[i]-1)
+		while (i<(len(leaders)-1) and leaders[i+1]==leaders[i]):
+			i+=1
+		if(i!=(len(leaders)-1)):
+			for j in range(i+1,leaders[i+1]):
+				tempBlock.append(j-1)
+		basicBlocks.append(tempBlock)
+	pprint.pprint(basicBlocks)
+
 
 if __name__=="__main__":
 	filename = sys.argv[1]
