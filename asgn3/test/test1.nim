a3dafd479e3b00e572d8b5aa26611f9dc4f7bc96
preetansh <preetg@iitk.ac.in>
2016-03-07 22:21:58 +0530

    first test case

--- asgn3/test/test1.nim
+++ asgn3/test/test1.nim
@@ -1,1 +1,15 @@
-rand()
+iterator items(range: int): int =
+  var i = range.low
+  while i <= range.high:
+    yield i
+
+iterator pairs(range: int): tuple[a: int, b: char] =
+  for i in range:  # uses Range.items
+    yield (i, char(i + ord('a')))
+
+for i, c in TRange(low: 1, high: 3):
+  echo c
+iterator items(range: int): int =
+  var i = range.low
+  while i <= range.high:
+    yield i
