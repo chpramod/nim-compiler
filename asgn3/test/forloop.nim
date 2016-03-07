8cdb2ab0da78de6b306b636547d5c7ee422ecfc2
chpramod <chpramod@cse.iitk.ac.in>
2016-03-07 23:09:52 +0530

    Test cases

--- asgn3/test/forloop.nim
+++ asgn3/test/forloop.nim
@@ -1,17 +1,15 @@
-type
-  TRange = object
-    low: int
-    high: int
-
-iterator items(range: TRange): int =
+iterator items(range: int): int =
   var i = range.low
   while i <= range.high:
     yield i
-    inc i
 
-iterator pairs(range: TRange): tuple[a: int, b: char] =
+iterator pairs(range: int): tuple[a: int, b: char] =
   for i in range:  # uses Range.items
     yield (i, char(i + ord('a')))
 
 for i, c in TRange(low: 1, high: 3):
   echo c
+iterator items(range: int): int =
+  var i = range.low
+  while i <= range.high:
+    yield i
