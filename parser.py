a92281d707b683f9219f5b96281fc49b8d7d213d
chpramod <chpramod@cse.iitk.ac.in>
2016-03-31 01:27:47 +0530

    parser grammar reverted to working version

--- parser.py
+++ parser.py
@@ -151,10 +151,10 @@ def p_exprStmt(p):
     # '''exprStmt : simpleExpr
     #             | lhs exprStmtInter
     #             | IDENTIFIER exprStmtInter'''
-    '''exprStmt : simpleExpr
-                | lhs EQUALS expr
-                | IDENTIFIER EQUALS expr'''
-
+    # '''exprStmt : simpleExpr
+    #             | lhs EQUALS expr
+    #             | IDENTIFIER EQUALS expr'''
+    '''exprStmt : simpleExpr exprStmtInter'''
     print "exprStmt has %d len"%(len(p))
     if len(p)==2:
         p[0]=p[1]
@@ -175,14 +175,14 @@ def p_exprStmt(p):
         print "=, %s, %s" %(p[1]['place'],p[3]['place'])
 
 #
-# def p_exprStmtInter(p):
-#     ''' exprStmtInter : EQUALS expr
-#                       | expr exprStmtInter2 doBlocks'''
-
+def p_exprStmtInter(p):
+    ''' exprStmtInter : EQUALS expr
+                      | expr exprStmtInter2 doBlocks
+                      | empty '''
 #
-# def p_exprStmtInter2(p):
-#     ''' exprStmtInter2 : COMMA expr exprStmtInter2
-#                        | empty '''
+def p_exprStmtInter2(p):
+    ''' exprStmtInter2 : COMMA expr exprStmtInter2
+                       | empty '''
 
 def p_whileStmt(p):
     '''whileStmt : WHILE condStmt'''
@@ -605,8 +605,8 @@ def p_interOne(p):
     '''interOne : OP0 arrowExpr interOne
                 | empty '''
     p[0] = {
-    'type:' ("OP0" if len(p) > 2 else None),
-    'value': p[1],
+    'type': ("OP0" if len(p) > 2 else None),
+    'value': p[1]
     }
     if len(p) > 2:
         msg_error(p,"Arrow like Operators not supported")
@@ -614,25 +614,25 @@ def p_interOne(p):
 def p_arrowExpr(p):
     '''arrowExpr : assignExpr
                 | assignExpr OP1 assignExpr'''
-    if len(p)==2:
-        p[0] = p[1]
-    else:
-        p[0] = {
-        'place': 'undef',
-        'type': 'ERROR_TYPE'
-        }
-        if p[1]['type']=='ERROR_TYPE' or p[3]['type']=='ERROR_TYPE':
-            msg_error(p,'Unsupported type')
-        elif p[1]['type']!=p[3]['type']:
-            msg_error(p,'Type mismatch')
-        else:
-            TAC.emit(p[2][0],p[1]['place'],p[1]['place'],p[3]['place'])
-            p[0] = p[1]
-        # p[0] = {
-        # 'type:' ("OP1" if len(p) > 2 else None),
-        # 'value': p[1],
-        # 'place'
-        # }
+    # if len(p)==2:
+    #     p[0] = p[1]
+    # else:
+    #     p[0] = {
+    #     'place': 'undef',
+    #     'type': 'ERROR_TYPE'
+    #     }
+    #     if p[1]['type']=='ERROR_TYPE' or p[3]['type']=='ERROR_TYPE':
+    #         msg_error(p,'Unsupported type')
+    #     elif p[1]['type']!=p[3]['type']:
+    #         msg_error(p,'Type mismatch')
+    #     else:
+    #         TAC.emit(p[2][0],p[1]['place'],p[1]['place'],p[3]['place'])
+    #         p[0] = p[1]
+    #     # p[0] = {
+    #     # 'type:' ("OP1" if len(p) > 2 else None),
+    #     # 'value': p[1],
+    #     # 'place'
+    #     # }
 
 # def p_interTwo(p):
 #     '''interTwo : OP1 assignExpr interTwo
@@ -647,8 +647,8 @@ def p_interThree(p):
     '''interThree : OP2 orExpr interThree
                 | empty '''
     p[0] = {
-    'type:' ("OP2" if len(p) > 2 else None),
-    'value': p[1],
+    'type': ("OP2" if len(p) > 2 else None),
+    'value': p[1]
     }
     if len(p) > 2:
         msg_error(p,p[1]+" operators not supported")
@@ -683,7 +683,7 @@ def p_interFour(p):
         msg_error(p,'Unsupported type')
     elif p[3]['place']==None:
         p[0] = {
-        'type': p[2]['type']
+        'type': p[2]['type'],
         'value': p[1],
         'place': p[2]['place']
         }
@@ -728,7 +728,7 @@ def p_interFive(p):
         msg_error(p,'Unsupported type')
     elif p[3]['place']==None:
         p[0] = {
-        'type': p[2]['type']
+        'type': p[2]['type'],
         'value': p[1],
         'place': p[2]['place']
         }
@@ -808,15 +808,15 @@ def p_identOrLiteral(p):
     #                   | literal
     #                   | par
     #                   | IDENTIFIER'''
-    # '''identOrLiteral :  literal
-    #                     | castExpr
-    #                     | arrayConstr
-    #                     | tupleConstr
-    #                     | symbol '''
     '''identOrLiteral :  literal
                         | castExpr
-                        | symbol
-                        | lhs'''
+                        | arrayConstr
+                        | tupleConstr
+                        | symbol '''
+    # '''identOrLiteral :  literal
+    #                     | castExpr
+    #                     | symbol
+    #                     | lhs'''
     p[0] = p[1]
 
 def p_lhs(p):
