f5f04e05b5cf427e2ee2fda52407471ffb08361c
VivekVerma <vivek@cse.iitk.ac.in>
2016-03-31 17:38:55 +0530

    threeAc updated

--- threeAC.py
+++ threeAC.py
@@ -5,15 +5,21 @@ class threeAC:
 		self.labelPrefix = 'l'
 		self.tempCount = -1
 		self.tempPrefix = '$t'
+
 	def emit(self,op,dest,src1,src2):
 		self.code.append([op,dest,src1,src2])
 
-	def createLabel(self):
+	def emitif(self,ifgoto,op,src1,src2,destlabel):
+		self.code.append([ifgoto,op,src1,src2,destlabel])
+
+	def newLabel(self):
 		self.labelCount += 1
 		return self.labelPrefix+str(self.labelCount)
+
 	def printCode(self):
 		for line in self.code:
 			print line
+
 	def createTemp(self):
 		self.tempCount += 1
 		return self.tempPrefix+str(self.tempCount)
