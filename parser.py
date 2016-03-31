d7fe7f5077e572cf35c2ce53b7b86d241901354f
revant <trevant@iitk.ac.in>
2016-04-01 01:12:51 +0530

    I fucked up somethings, now fixed

--- parser.py
+++ parser.py
@@ -10,18 +10,11 @@ import threeAC
 # tokens = lexer.tok_data
 import lexer
 tokens = lexer.tokens
-
-identifier = {}
-identifierList = []
-
-
-# initialising TAC GLOBALLY
+import threeAC
 
 TAC = threeAC.ThreeAC()
-
-
-
-
+identifier = {}
+identifierList = []
 
 def p_start(p):
 #ignored extra
@@ -275,36 +268,30 @@ def p_finallyInter(p):
 def p_markerlabel(p):
     '''markerlabel : empty'''
     p[0] = {
-    'label': ST.newLabel()
+    'label': TAC.newLabel()
     }
+    # TAC.emit('label'  ,p[0]['label'],'','')
 
 def p_markerif(p):
     '''markerif : empty'''
-    if p[-1]['type']!='BOOLEAN':
+    if p[-1]['cond']['type']!='BOOLEAN':
         msg_error(p,'Condition expressions must be boolean type')
-    TAC.emit('ifgoto','==',p[-1]['cond']['place'],'0',p[-1]['truelabel'])
-    TAC.emit('goto',p[-1]['falselabel'])
-    
+
+    # TAC.emit('ifgoto','==',p[-1]['cond']['place'],'0',p[-1]['truelabel'])
+    # TAC.emit('goto',p[-1]['falselabel'],'','')
+
+def p_markerjump(p):
+    '''markerjump : empty'''
+    # TAC.emit('goto',p[-1]['endlabel'],'','')
 
 def p_markerend(p):
     '''markerend : empty'''
     p[0] = {
-    'label': ST.newLabel()
-    }
-
-def p_ifStmt(p):
-    '''ifStmt : IF condStmt markerif elifStmt elseStmt ifEndLabel'''
-    p[0] = {
-    'inline': False,
-    'type': p[1],
-    'cond': p[2]['cond'],
-    'then': p[2]['then'],
-    'elif': p[4],
-    'else': p[5]
+    'label': TAC.newLabel()
     }
 
 def p_whenStmt(p):
-    '''whenStmt : WHEN condStmt markerif elifStmt elseStmt markerend'''
+    '''whenStmt : WHEN condStmt markerif elifStmt elseStmt'''
     p[0] = {
     'inline': False,
     'type': p[1],
@@ -313,41 +300,33 @@ def p_whenStmt(p):
     'elif': p[4],
     'else': p[5]
     }
-    if p[2]['type']!='BOOLEAN':
-        msg_error(p,'Condition expressions must be boolean type')
-    TAC.emit('ifgoto','==',p[2]['cond']['place'],'0',p[2]['truelabel'])
-    TAC.emit('goto',p[2]['falselabel'])
+    # if p[2]['type']!='BOOLEAN':
+    #     msg_error(p,'Condition expressions must be boolean type')
+    #     TAC.emit('ifgoto','==',p[2]['cond']['place'],'0',p[2]['truelabel'])
+    #     TAC.emit('goto',p[2]['falselabel'])
+
+def p_ifStmt(p):
+    '''ifStmt : IF markerend condStmt markerif elifStmt elseStmt'''
+    p[0] = {
+    'inline': False,
+    'type': p[1],
+    'cond': p[3]['cond'],
+    'then': p[3]['then'],
+    'elif': p[5],
+    'else': p[6]
+    }
+    # TAC.emit('label',p[1]['label'],'','')
 
 def p_condStmt(p):
-    '''condStmt : expr COLON makeCondLabels1 suite endCondLabel'''
-    # p[0] = {
-    # 'inline': False,
-    # 'cond': p[1],
-    # 'then': p[4],
-    # 'truelabel': p[3]['label'],
-    # 'falselabel': p[5]['label']
-    # }
-
-def p_makeCondLabels1(p):
-    ''' makeCondLabels1 :  '''
-    label1 = TAC.makeLabel()
-    label2 = TAC.makeLabel()
-    label3 = TAC.makeLabel()
-    p[0]=[label1,label2,label3]
-    TAC.emit('ifgoto', 'eq', p[-2]['place'], 1, 'goto', label1)
-    TAC.emit('goto', label2, '', '')
-    TAC.emit('label', label1, '', '')
-
-def p_endCondLabel(p):
-    ''' endCondLabel :  '''
-    TAC.emit('goto', p[-2][2], '', '')
-    TAC.emit('label', p[-2][1], '', '')
-
-
-def p_ifEndLabel(p):
-    ''' ifEndLabel : '''
-
-
+    '''condStmt : expr COLON markerlabel suite markerjump markerlabel'''
+    p[0] = {
+    'inline': False,
+    'cond': p[1],
+    'then': p[4],
+    'falselabel': p[6]['label'],
+    'truelabel': p[3]['label'],
+    # 'endlabel': p[-1]['label']
+    }
 
 def p_elseStmt(p):
     '''elseStmt : ELSE COLON suite
@@ -576,7 +555,12 @@ def p_strings(p):
     '''strings : STRLIT
                 | RSTRLIT
                 | TRIPLESTRLIT'''
-    p[0] = p[1]
+    temp = TAC.createTemp()
+    TAC.emit('=',temp,p[1],'')
+    p[0]={
+    'type': 'STRING',
+    'place': temp
+    }
 
 def p_expr(p):
     '''expr : ifExpr
@@ -810,18 +794,19 @@ def p_cmpExpr(p):
         msg_error(p,'Unsupported type')
     elif p[1]['type']!=p[2]['type']:
         msg_error(p,'Type mismatch')
-    elif p[1]['type']=='BOOLEAN':        
+    elif p[1]['type']=='BOOLEAN':
         msg_error(p,"Boolean not allowed in comparision statements")
     else:
         temp = TAC.createTemp()
         label1 = TAC.newLabel()
         label2 = TAC.newLabel()
-        TAC.emit('ifgoto',p[2]['value'],p[1]['place'],p[2]['place'],label1['name'])
-        TAC.emit('=', temp, 0)
-        TAC.emit("goto", label2['name'])
-        TAC.emit("label", label1['name'])
-        TAC.emit('=', temp, 1)
-        TAC.emit("label", label2['name'])
+        # print p[1],p[2],"hi"
+        TAC.emitif('ifgoto',p[2]['value'],p[1]['place'],p[2]['place'],label1)
+        TAC.emit('=', temp, 0,'')
+        TAC.emit("goto", label2,'','')
+        TAC.emit("label", label1,'','')
+        TAC.emit('=', temp, 1,'')
+        TAC.emit("label", label2,'','')
         p[0] = {
         'type': 'BOOLEAN',
         'place': temp
@@ -1131,7 +1116,7 @@ def p_interPrefixOperator(p):
         }
     else :
         msg_error(p,'currently interPrefixOperator -> empty')
-        
+
 def p_interPrimarySuffix(p):
     '''interPrimarySuffix : primarySuffix interPrimarySuffix
                             | empty '''
@@ -1160,12 +1145,10 @@ def p_identOrLiteral(p):
     #                     | lhs'''
 
     p[0] = p[1]
-
-    print p[1], "^^&&&&^^"
     temp = TAC.createTemp()
-    TAC.emit('=',temp,p[1],'','')
+    TAC.emit('=',temp,p[1],'')
     p[0] = {
-    'type': p[1]['type'],
+    'type': (p[1]['type'] if p[1]!=None else None),
     'value': p[1],
     'place': temp
         }
@@ -1253,10 +1236,15 @@ def p_symbol(p):
                 | ADDR
                 | TYPE
                 | BOOLEAN'''
-    p[0] = p[1]
+    temp = TAC.createTemp()
+    TAC.emit('=',temp,p[1],'')
+    p[0] = {
+    'type': p.slice[1].type,
+    'place': temp
+    }
 
 def p_literal(p):# was INTLIT in place of INT
-    '''literal : int             
+    '''literal : INTLIT
                 | INT8LIT
                 | INT16LIT
                 | INT32LIT
@@ -1267,10 +1255,15 @@ def p_literal(p):# was INTLIT in place of INT
                 | CHARLIT
                 | strings
                 | NIL'''
-    
-    p[0] = p[1]
 
-    
+    temp = TAC.createTemp()
+    TAC.emit('=',temp,p.slice[1].type,'')
+    p[0] = {
+    'type': 'int',
+    'value': p[1],
+    'place': temp
+    }
+
 # def p_par(p):
 
 def p_int(p):
@@ -1572,6 +1565,7 @@ def testYacc(inputFile):
     customLexer = lexer.customLexer()
     result=parser.parse(data, lexer=customLexer, debug=log)
     pprint(result)
+    TAC.printCode()
     # parser.parse(program, lexer=lexer, debug=1)
 
 if __name__ == "__main__":
@@ -1580,7 +1574,7 @@ if __name__ == "__main__":
     testYacc(inputFile)
 
     # to print TAC code
-    TAC.printcode()
+    TAC.printCode()
 
 
     #code to get reduced rules as an output file
