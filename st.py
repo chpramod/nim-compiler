1ecd7a5cb14eb8c269f2088583537ede9d637cd7
Preetansh <preetg@iitk.ac.in>
2016-04-02 07:45:38 +0530

    some functions added

--- st.py
+++ st.py
@@ -28,3 +28,39 @@ class St:
     def getNewBlockName(self):
         self.curBlockNo+=1
         return 'b'+str(self.curBlockNo)
+
+    def addIden(self, idenName, place, idenType):
+        self.St[self.curScope]['identifiers'][idenName]={
+            'place'=place,
+            'type'=idenType
+        }
+
+    def getIden(self, idenName):
+        idenScope = self.getIdenScope(idenName)
+        if idenScope!=None:
+            #returns all attributes of identifier
+            return self.St.[idenScope]['identifiers'][idenName].get(idenName)
+
+        else:
+            return None
+
+    def getIdenScope(self, idenName):
+        scope=self.curScope
+        while scope!=None:
+            if idenName in self.St[scope]['identifiers']:
+                return scope
+            scope=self.St[scope]['parent']
+
+        return None
+
+    def getIdenAttr(self, idenName, attrName):
+        idenScope = self.getIdenScope(idenName)
+        if idenScope!=None:
+            return self.St.[idenScope]['identifier'][idenName].get(attrName)
+        else:
+            return None
+
+    def setidenAttr(self, idenName, attrName, attrVal):
+        idenScope = self.getIdenScope(idenName)
+        if idenScope!=None:
+            self.St.[idenScope]['identifiers'][idenName][attrName]=attrVal
