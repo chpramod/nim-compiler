423aeba41225e8e6cabdb143bb44a6e3c7c0eec7
revant <trevant@iitk.ac.in>
2016-04-04 23:36:33 +0530

    test folder added

---
+++ asgn4/test/test5.nim
@@ -0,0 +1,16 @@
+#[ Multiline comment in already
+   commented out code. ]#
+
+echo "words \\ words words "
+echo """
+<html>
+  <head>
+  </head>
+  <body>
+  </body>
+</html> """
+
+proc re(s: system.string): system.string = s
+
+echo r".""."
+
