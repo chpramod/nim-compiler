63e6aea688f13bb08fcf8c7d80927781ff668ec6
Preetansh <preetg@iitk.ac.in>
2016-04-11 18:35:37 +0530

    testcases for demo

---
+++ testcaseAss3And4/for.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+    int i=6;
+    for (;i<= 8 && i>= 6 && i!= 7; i++){
+        if (i>=0){
+            printf("yes\n");
+        }
+        else
+            printf("no\n");
+    }
+    return 0;
+}
+
+
+var i:int=6
+while i<=8 and i>=6 and i!=7:
+  if i>=0:
+    echo 1
+  else:
+    echo 0
+  i = i+1
